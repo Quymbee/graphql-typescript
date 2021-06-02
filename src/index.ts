07f9c1d216c84cc6d7c49b2f034b8d926d6ae407
Quymbee Chen <quymbee@mattchen.com>
2021-06-02 10:11:50 -0400

    mikro orm

--- src/index.ts
+++ src/index.ts
@@ -6,9 +6,11 @@ import mikroConfig from "./mikro-orm.config";
 const main = async () => {
   const orm = await MikroORM.init(mikroConfig);
   await orm.getMigrator().up();
-  const post = orm.em.create(Post, { title: "my first post" });
-  await orm.em.persistAndFlush(post);
-  await orm.em.nativeInsert(Post, { title: "second post" });
+  // const post = orm.em.create(Post, { title: "my first post" });
+  // await orm.em.persistAndFlush(post);
+
+  // const posts = await orm.em.find(Post, {});
+  // console.log(posts);
 };
 
 main().catch((err) => console.error(err));
